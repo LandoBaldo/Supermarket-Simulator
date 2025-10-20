2f7b36536664edb73b19c6b34443a3ec6c793996
niceu11 <gafamek@gmail.com>
2025-10-20 21:50:44 +0800

    Update Equipment.java

--- Equipment.java
+++ Equipment.java
@@ -1,14 +1,41 @@
 import java.util.ArrayList;
-public class Equipment {
+
+class Equipment {
     protected int capacity;
-    protected ArrayList<Product> products;
+    protected ArrayList<Products> products;
 
     public Equipment(int capacity) {
         this.capacity = capacity;
+        this.products = new ArrayList<>();
     }
 
     public int getCapacity() {
         return capacity;
     }
 
+    public boolean addProduct(Products product) {
+        if (products.size() < capacity) {
+            products.add(product);
+            System.out.println("Product " + product.getName() + " added to equipment.");
+            return true;
+        }
+        System.out.println("Equipment full! Cannot add more products. Capacity: " + capacity);
+        return false;
+    }
+
+    public ArrayList<Products> getProducts() {
+        return products;
+    }
+
+    public void displayProducts() {
+        System.out.println("Products in equipment:");
+        for (Products product : products) {
+            System.out.println("  - " + product);
+        }
+    }
+
+    public double calculateTotalValue() {
+        // Placeholder - you can add price field to Products later
+        return products.size() * 10.0;
+    }
 }
