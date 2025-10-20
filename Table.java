a21ade0e3e60d550d27d2a8502611813f18e1322
niceu11 <gafamek@gmail.com>
2025-10-20 21:47:44 +0800

    Update Table.java

--- Table.java
+++ Table.java
@@ -1,8 +1,6 @@
 import java.util.ArrayList;
-public class Table extends StorageUnit {
-    private int capacity;
-    private ArrayList<Product> products;
 
+class Table extends StorageUnit {
     public Table(int capacity) {
         super(capacity);
     }
@@ -11,5 +9,15 @@ public class Table extends StorageUnit {
         this(4);
     }
 
-    
+    // Display products specifically for table
+    public void arrangeProducts() {
+        System.out.println("\n--- Arranging products on Table ---");
+        if (products.isEmpty()) {
+            System.out.println("Table is empty.");
+        } else {
+            for (Products p : products) {
+                System.out.println("  " + p.getName() + " placed on table at position");
+            }
+        }
+    }
 }
