693669076badd32cce5100ede042adcb0ce40aaf
niceu11 <gafamek@gmail.com>
2025-10-20 21:50:20 +0800

    Update Refrigerator.java

--- Refrigerator.java
+++ Refrigerator.java
@@ -1,19 +1,33 @@
 import java.util.ArrayList;
-public class Refrigerator extends StorageUnit{
-    private int capacity;
-    private ArrayList<Product> product;
+
+class Refrigerator extends StorageUnit {
+    private double temperature;
 
     public Refrigerator(int capacity) {
         super(capacity);
+        this.temperature = 4.0; // Default refrigerator temp
     }
+
     public Refrigerator() {
         this(9);
     }
 
     public void setTemperature(double temperature) {
-    this.temperature = temperature;
-    System.out.println("Temperature set to: " + temperature + "°C");   
+        this.temperature = temperature;
+        System.out.println("Temperature set to: " + temperature + "°C");
     }
-    
-}
 
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public boolean addProductWithCheck(Products product) {
+        // Check if product should be in refrigerator
+        if (!product.getDisplayLocation().equals("Refrigerator")) {
+            System.out.println("Warning: " + product.getName() +
+                    " is meant for " + product.getDisplayLocation() +
+                    ", not Refrigerator!");
+        }
+        return super.addProduct(product);
+    }
+}
