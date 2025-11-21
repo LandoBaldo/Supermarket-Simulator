1abe7b44f9734ec6c0e2523ab3ae39753a846806
LandoBaldo <landog.kaldog@gmail.com>
2025-11-21 14:24:19 +0800

    Update Basket.java

--- Basket.java
+++ Basket.java
@@ -1,11 +1,11 @@
 import java.util.ArrayList;
 
+import Base.Equipment;
 class Basket extends Equipment {
     public Basket(int capacity) {
         super(capacity);
     }
-
     public Basket() {
         this(15);
     }
-}
+}   
