2eaa04d55a4f5299e33d8742269960de862a1dfd
LandoBaldo <landog.kaldog@gmail.com>
2025-11-21 14:20:39 +0800

    Update Table.java

--- Table.java
+++ Table.java
@@ -1,3 +1,7 @@
+
+import Base.Product;
+import Base.StorageUnit;
+
 import java.util.ArrayList;
 
 class Table extends StorageUnit {
@@ -9,13 +13,13 @@ class Table extends StorageUnit {
         this(4);
     }
 
-    // Display products specifically for table
-    public void arrangeProducts() {
-        System.out.println("\n--- Arranging products on Table ---");
+    // Display Product specifically for table
+    public void arrangeProduct() {
+        System.out.println("\n--- Arranging Product on Table ---");
         if (products.isEmpty()) {
             System.out.println("Table is empty.");
         } else {
-            for (Products p : products) {
+            for (Product p : products) {
                 System.out.println("  " + p.getName() + " placed on table at position");
             }
         }
