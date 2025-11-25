fecd5414aed3caaa9d9d483df75b5a3b6f14908e
LandoBaldo <landog.kaldog@gmail.com>
2025-11-25 18:59:26 +0800

    Copied Patch 1.0.2

--- Base/Equipment.java
+++ Base/Equipment.java
@@ -1,53 +1,51 @@
-package Base;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Gabriel
+ */
+import Base.Product;
 import java.util.ArrayList;
 
-public class Equipment {
+public abstract class Equipment {
     protected int capacity;
-    protected ArrayList<Product> products;
+    protected ArrayList<Product> contents;
 
     public Equipment(int capacity) {
         this.capacity = capacity;
-        this.products = new ArrayList<>();
+        this.contents = new ArrayList<>();
     }
 
     public int getCapacity() {
         return capacity;
     }
 
-    public boolean addProduct(Product product) {
-        if (products.size() < capacity) {
-            products.add(product);
-            System.out.println("Product " + product.getName() + " added to equipment.");
+    // Matches Shopper.java call: equipment.addProduct(p)
+    public boolean addProduct(Product p) {
+        if (contents.size() < capacity) {
+            contents.add(p);
             return true;
         }
-        System.out.println("Equipment full! Cannot add more products. Capacity: " + capacity);
+        System.out.println("Equipment is full! (Max " + capacity + ")");
         return false;
     }
 
-    public ArrayList<Product> getProducts() {
-        return products;
+    public void removeProduct(Product p) {
+        contents.remove(p);
+    }
+    
+    public void clear() {
+        contents.clear();
     }
 
-    public void displayProducts() {
-        System.out.println("Products in equipment:");
-        for (Product product : products) {
-            System.out.println("  - " + product);
-        }
+    public ArrayList<Product> getContents() {
+        return contents;
     }
-
-    public double calculateTotalValue() {
-        double total = 0;
-        for (Product product : products) {
-            total += product.getPrice();
-        }
-        return total;
-    }
-
-    public boolean removeProduct(Product product) {
-        return products.remove(product);
-    }
-
+    
     public boolean isEmpty() {
-        return products.isEmpty();
+        return contents.isEmpty();
     }
 }
